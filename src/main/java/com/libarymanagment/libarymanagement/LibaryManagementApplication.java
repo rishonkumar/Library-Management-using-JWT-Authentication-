e3bcdbc7e8cab59ef9f2ea9f23c230effe77dec9
Rishon <rishonkumarrahi@gmail.com>
2025-02-17 19:08:56 +0530

    Initial Project Setup

---
+++ src/main/java/com/libarymanagment/libarymanagement/LibaryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.libarymanagment.libarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibaryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibaryManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/libarymanagment/libarymanagement/LibaryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libarymanagment.libarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibaryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
