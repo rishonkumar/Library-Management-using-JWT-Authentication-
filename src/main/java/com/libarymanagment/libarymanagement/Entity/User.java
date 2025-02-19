cab666ee0bcd8e05400eae1ffda2a1bd07810eeb
rishonkumar <rishon.kumar.rahi@gmail.com>
2025-02-19 22:15:10 +0530

    Adding CustomerUserDetailService for initial authentication of the user

--- src/main/java/com/libarymanagment/libarymanagement/Entity/User.java
+++ src/main/java/com/libarymanagment/libarymanagement/Entity/User.java
@@ -2,13 +2,18 @@ package com.libarymanagment.libarymanagement.Entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @Data
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +26,16 @@ public class User {
     //this will create a new table in DB with column name id and roles and
     // EAGER because data will be laoded immediately when the user entity is feteched from the DB
     private Set<String> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //For admin we are giving 2 roles ROLE_USER and ROLE_ADMIN so we need to wrap both the
+        //roles in SimpleGrantedAuthority
+        return roles.stream().map(SimpleGrantedAuthority::new).toList();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
---
+++ src/main/java/com/libarymanagment/libarymanagement/Service/CustomerUserDetailService.java
@@ -0,0 +1,22 @@
+package com.libarymanagment.libarymanagement.Service;
+
+import com.libarymanagment.libarymanagement.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//This class will take care of initial authentication of the user
+// then it send to JWT to generate token
+@Service
+public class CustomerUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
